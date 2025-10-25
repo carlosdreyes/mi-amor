5362149366c29bdebf42c88e493ae3cef02dcdcb
carlosdreyes <carlosdreyes1601@gmail.com>
2025-10-25 13:30:58 -0600

    Delete historia.js

--- historia.js
+++
@@ -1,9 +0,0 @@
-
-function historia() {
-let resultadoDiv = document.getElementById("r");
-
-resultadoDiv.textContent = "Recuerdo cuando te vi en el salón de ciencias; me pareciste hermosísima y, desde ahí, empezó todo. Cuando te mandé solicitud, me sentía muy nervioso, la verdad, pero en cuanto vi que aceptaste, se convirtió en felicidad.\n\nLuego, cuando empezamos a hablar, me enamoré perdidamente de ti y de tu forma de tratarme. Nunca me había sentido tan especial como me haces sentir.\n\nCuando descubrí que tú también sientes lo mismo, moría de felicidad. Desde ese momento me hiciste el hombre más feliz del mundo, aunque me frustré un poquito cuando me enteré de que no podíamos ser nada aún. Pero la espera valió totalmente la pena, ya que eres lo mejor que me ha pasado en la vida.\n\nCuando empezamos a andar, me fui a mi casa tan emocionado de estar con el amor de mi vida que no podía creer estar contigo. En nuestra primera salida yo estaba más nervioso que nunca; recuerdo lo hermosa que te veías ese día y que ese día te di tu anillo de promesa. Recuerdo muy bien que fue un 16 de junio, ya que ese día marcó mi vida casi tanto como el 10, que fue el día que empezamos a estar juntos.\n\nDesde que te conocí, me has hecho más y más feliz, y sé que esta historia tendrá mucho más por escribirse.\nTe amo con todo mi ser. <3";
-
-
-
-}
